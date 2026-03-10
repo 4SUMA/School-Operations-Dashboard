c21c2b45b0a10f30dcbb818b3f5888a97161d5b1
Suma R <109577092+4SUMA@users.noreply.github.com>
2026-03-10 20:15:13 +0530

    file uploaded
    file uploaded

---
+++ index.js
@@ -0,0 +1,21 @@
+const attendanceData = {
+  totalStudents: 1200,
+  present: 1080,
+  absent: 120
+};
+
+const feeData = {
+  totalFees: 5000000,
+  collected: 3800000,
+  pending: 1200000
+};
+
+const examResults = [
+  { subject: "Math", average: 72 },
+  { subject: "Science", average: 68 },
+  { subject: "English", average: 75 }
+];
+
+console.log(attendanceData);
+console.log(feeData);
+console.log(examResults);
